947884da83d9e51639854bdc5a8c269a475c5606
qhtkdrma <kimwonkyu15@gmail.com>
2022-03-24 12:20:15 +0900

    standard code

--- src/main/java/com/univercityweb/server/ServletInitializer.java
+++ src/main/java/com/univercityweb/server/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.univercityweb.server;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
