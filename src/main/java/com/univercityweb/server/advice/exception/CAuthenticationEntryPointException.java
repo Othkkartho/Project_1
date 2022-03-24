cf2624c0460bfd1a350b1bad29bdf4ff78d266a1
qhtkdrma <kimwonkyu15@gmail.com>
2022-03-24 12:16:59 +0900

    error division

---
+++ src/main/java/com/univercityweb/server/advice/exception/CAuthenticationEntryPointException.java
@@ -0,0 +1,15 @@
+package com.univercityweb.server.advice.exception;
+
+public class CAuthenticationEntryPointException extends RuntimeException {
+    public CAuthenticationEntryPointException() {
+        super();
+    }
+
+    public CAuthenticationEntryPointException(String message) {
+        super(message);
+    }
+
+    public CAuthenticationEntryPointException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
